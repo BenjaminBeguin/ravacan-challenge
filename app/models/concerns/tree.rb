41e73af904f13f290747a4c5860735c78825dcd4
Paul Maltsev <maltsevp@gmail.com>
2020-11-27 12:04:42 -0800

    adding hierarchical gem and Tree module

---
+++ app/models/concerns/tree.rb
@@ -0,0 +1,72 @@
+module Tree extend ActiveSupport::Concern
+
+  def build_tree id
+    edges       = get_edges(id)
+    uniq_edges  = edges.distinct
+    @ids        = edges.pluck(:subcomponent_id) << id #get IDs of all components for price calculation.
+    @components = get_components(@ids).group_by(&:id)
+    create_tree(uniq_edges, id) #pass distinct edges only, to avoid duplication.
+  end
+
+  def get_edges id
+    ComponentToComponent.join_recursive do |query|
+      query
+        .start_with(component_id: id) { select('0 depth') }
+        .connect_by(subcomponent_id: :component_id)
+        .select(query.prior[:depth] + 1, start_with: false)
+    end.order('depth ASC')
+  end
+
+  def create_tree edges, root_id
+    adj_list           = get_adj_list(edges)
+    tree               = self.attributes #product attrs.
+    tree["price"]      = get_price
+    tree["components"] = [] << get_subtree(adj_list, root_id, {})
+    return tree
+  end
+
+  def get_adj_list edges
+    edges.pluck(:component_id, :subcomponent_id).group_by {|e| e[0]}
+  end
+  
+  #can be done iteratively if needed.
+  def get_subtree adj_list, node, memo
+    return memo[node] if memo[node] # avoiding repeated subtree creation.
+
+    subtree               = {}
+    subtree["name"]       = @components[node].first.name
+    subtree["components"] = []
+
+    edges = adj_list[node] # array of edges for current node.
+    return subtree if edges.nil?
+
+    edges.each do |e|
+      child = e[1]
+      subtree["components"] << get_subtree(adj_list, child, memo)
+    end
+    memo[node] = subtree
+    subtree
+  end
+
+  def get_components ids
+    Component.where(id: ids)
+  end
+
+  def get_price
+    price          = 0
+    uniq_ids       = @ids.uniq
+    component_freq = get_frequency(@ids)
+
+    uniq_ids.each do |id|
+      price += @components[id].first.price * component_freq[id]
+    end
+    price
+  end
+
+  def get_frequency ids
+    frequencies = Hash.new(0)
+    ids.each{|key| frequencies[key] += 1}
+    return frequencies
+  end
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,18 @@ class Product < ApplicationRecord
   has_one :root_component, -> { where(components: { isRoot: true } ) },
     through: :product_to_component, source: :component
   #TODO: Delete root_component on product delete.
+
+  after_create :create_root_component
+
+  include Tree
+
+  def get_tree
+    product_root = self.root_component #if doesn't exist, redirect to 404.
+    build_tree(product_root.id)
+  end
+
+  private
+  def create_root_component
+    self.root_component = Component.create(name: self.name, isRoot: true)
+  end
 end
